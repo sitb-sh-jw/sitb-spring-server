ef1f1319fe520a380e565f62f9918e6ce22dc7a4
Rudolf0328 <ck07160@hanyang.ac.kr>
2024-05-13 17:43:43 +0900

    [Feature] week api

--- src/main/java/com/sitb/shjw/sitb/Controller/ScheduleController.java
+++ src/main/java/com/sitb/shjw/sitb/Controller/ScheduleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -42,30 +43,51 @@ public class ScheduleController {
                 ScheduleResponse scheduleResponse = new ScheduleResponse(schedule.getTitle(), category.getName(), category.getColor(), schedule.getStart_time(), schedule.getEnd_time(), schedule.getLocation(), schedule.getMemo());
                 if (schedule.getStart_time().getDay() == sDate.getDay()) {
                     List<ScheduleResponse> mondayScheduleList = scheduleWeekResponse.getMonday();
+                    if (mondayScheduleList == null || mondayScheduleList.isEmpty()) {
+                        mondayScheduleList = new ArrayList<>();
+                    }
                     mondayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(mondayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+1) {
                     List<ScheduleResponse> tuesdayScheduleList = scheduleWeekResponse.getTuesday();
+                    if (tuesdayScheduleList == null || tuesdayScheduleList.isEmpty()) {
+                        tuesdayScheduleList = new ArrayList<>();
+                    }
                     tuesdayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(tuesdayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+2) {
                     List<ScheduleResponse> wednesdayScheduleList = scheduleWeekResponse.getWednesday();
+                    if (wednesdayScheduleList == null || wednesdayScheduleList.isEmpty()) {
+                        wednesdayScheduleList = new ArrayList<>();
+                    }
                     wednesdayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(wednesdayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+3) {
                     List<ScheduleResponse> thursdayScheduleList = scheduleWeekResponse.getThursday();
+                    if (thursdayScheduleList == null || thursdayScheduleList.isEmpty()) {
+                        thursdayScheduleList = new ArrayList<>();
+                    }
                     thursdayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(thursdayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+4) {
                     List<ScheduleResponse> fridayScheduleList = scheduleWeekResponse.getFriday();
+                    if (fridayScheduleList == null || fridayScheduleList.isEmpty()) {
+                        fridayScheduleList = new ArrayList<>();
+                    }
                     fridayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(fridayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+5) {
                     List<ScheduleResponse> saturdayScheduleList = scheduleWeekResponse.getSaturday();
+                    if (saturdayScheduleList == null || saturdayScheduleList.isEmpty()) {
+                        saturdayScheduleList = new ArrayList<>();
+                    }
                     saturdayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(saturdayScheduleList);
                 } else if (schedule.getStart_time().getDay() == sDate.getDay()+6) {
                     List<ScheduleResponse> sundayScheduleList = scheduleWeekResponse.getSunday();
+                    if (sundayScheduleList == null || sundayScheduleList.isEmpty()) {
+                        sundayScheduleList = new ArrayList<>();
+                    }
                     sundayScheduleList.add(scheduleResponse);
                     scheduleWeekResponse.setMonday(sundayScheduleList);
                 }
